ae23ff2e1238ec2f1075676e7c9736426f352509
Temporary Account <guest07@borg16.cs.purdue.edu>
2018-06-13 16:02:39 -0400

    Add mergesort file

---
+++ merge.py
@@ -0,0 +1,32 @@
+def merge(left, right):
+    merged = []
+    leftInd = 0
+    rightInd = 0
+    while leftInd < len(left) and rightInd < len(right):
+        if left[leftInd] <= right[rightInd]:
+            merged.append(left[leftInd])
+            #left = left[:len(left)//2]
+            leftInd+=1
+        else:
+            merged.append(right[rightInd])
+            #right = right[len(right)//2:]
+            rightInd+=1
+
+    merged+=left[leftInd:]
+    merged+=right[rightInd:]
+    return merged
+    
+def sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr)//2
+    left = sort(arr[:mid])
+    right = sort(arr[mid:])
+
+    return merge(left,right)
+
+arr = [35,27,16,76,43,89,38,-2,28,53,99]
+
+
+sortedList = sort(arr)
+print(sortedList)
